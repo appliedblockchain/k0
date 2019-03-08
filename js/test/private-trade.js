aa63d9c917531eaa4e1c373e30f936e495dbba2e
Mario Gemoll <mario@appliedblockchain.com>
2019-03-08 20:55:22 +0000

    Private trade 1st step: deposit

--- js/test/private-trade.js
+++ js/test/private-trade.js
@@ -1,4 +1,5 @@
 const assert = require('assert')
+const crypto = require('crypto')
 const util = require('./util')
 const sendTransaction = require('../send-transaction')
 const log = console.log
@@ -6,55 +7,110 @@ const write = x => process.stdout.write(x)
 const printState = require('./helpers/print-state')
 const expect = require('code').expect
 const BN = require('bn.js')
+const compileContracts = require('./helpers/compile-contracts')
+const commonTradingInit = require('./helpers/common-trading-init')
+const jayson = require('jayson/promise')
+const makeClient = require('../client')
+const flattenProof = require('../flatten-proof')
 
 describe('Transparent trade', async () => {
 
-  // the web3 object
-  let web3
-
-  // map accountIdentifier => web3 account
-  let accounts
-
-  // map address => account name
-  let accountNames
-
-  // list of car ids in the system (for display purposes)
-  let carIds
-
-  // DollarCoin minter account
-  let tokenMaster
-
-  // CarToken minter account
-  let carManufacturer
-
-  // contract object for ERC-20 token
-  let dollarCoin
-
-  // contract object for ERC-721 token
-  let carToken
+  let web3, accounts, accountNames, carIds, tokenMaster, carManufacturer,
+    artefacts, dollarCoin, carToken, k0Coin, client
 
   before(async () => {
-    web3 = util.initWeb3()
-    accounts = {}
-    accountNames = {}
-    carIds = []
-    const names = [
-      ['alice', 'Alice'],
-      ['bob', 'Bob']
-    ].forEach(pair => {
-      const account = web3.eth.accounts.create()
-      accounts[pair[0]] = account
-      accountNames[account.address] = pair[1]
-    })
+    const initResult = await commonTradingInit()
+    web3 = initResult.web3
+    accounts = initResult.accounts
+    accountNames = initResult.accountNames
+    carIds = initResult.carIds
+    tokenMaster = initResult.tokenMaster
+    carManufacturer = initResult.carManufacturer
+    artefacts = initResult.artefacts
+    dollarCoin = initResult.dollarCoin
+    carToken = initResult.carToken
 
-    tokenMaster = web3.eth.accounts.create()
-    carManufacturer = web3.eth.accounts.create()
-    const artefacts = await compileContracts()
-    dollarCoin = await util.deployContract(web3, artefacts.DollarCoin, [], tokenMaster)
-    carToken = await util.deployContract(web3, artefacts.CarToken, [], carManufacturer)
+    server = makeClient()
+    await server.ready()
+    await server.reset()
+    const initialRoot = await server.root()
+    const verifierAddresses = await Promise.all([
+      'CommitmentVerifier',
+      'AdditionVerifier',
+      'TransferVerifier',
+      'WithdrawalVerifier'
+    ].map(async name => {
+      const contract = await util.deployContract(web3, artefacts[name])
+      return contract._address
+    }))
+    k0Coin = await util.deployContract(
+      web3,
+      artefacts.MVPPT,
+      [
+        dollarCoin._address,
+        ...verifierAddresses,
+        await util.pack256Bits(initialRoot)
+      ]
+    )
   })
 
+  function sampleCoin(v) {
+    assert(BN.isBN(v))
+    return {
+      rho: util.randomBytes(32),
+      r: util.randomBytes(48),
+      a_sk: util.randomBytes(32),
+      v
+    }
+  }
+
   it('Full cycle', async () => {
-    log('hi')
+    // bob: Pay in 2x
+    printState(dollarCoin, carToken, accounts, accountNames, carIds)
+
+
+    const coins = [
+      sampleCoin(new BN("30000")),
+      sampleCoin(new BN("40000"))
+    ]
+    for (let i = 0; i < 2; i++) {
+
+      const c = coins[i]
+      await sendTransaction(
+        web3,
+        dollarCoin._address,
+        dollarCoin.methods.approve(k0Coin._address, c.v.toString()).encodeABI(),
+        5000000,
+        accounts.bob
+      )
+
+      const a_pk = await server.prf_addr(c.a_sk)
+      console.log(a_pk)
+      const data = await server.prepare_deposit(a_pk, c.rho, c.r, c.v.toString())
+      const commitmentProofCompact = flattenProof(data.commitmentProof)
+      const additionProofCompact = flattenProof(data.additionProof)
+
+      const params = [
+        c.v.toString(),
+        await util.pack256Bits(data.k),
+        await util.pack256Bits(data.cm),
+        await util.pack256Bits(data.nextRoot),
+        commitmentProofCompact,
+        additionProofCompact
+      ]
+      const receipt = await sendTransaction(
+        web3,
+        k0Coin._address,
+        k0Coin.methods.deposit(...params).encodeABI(),
+        5000000,
+        accounts.bob
+      )
+
+      assert(receipt.status)
+
+      server.add(data.cm)
+
+    }
+
   })
 })
