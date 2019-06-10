2dcf7b65e416583824e6c42c0d6703bf5ba9e8b3
Mario Gemoll <mario@appliedblockchain.com>
2019-06-10 15:33:02 +0100

    removed debug output [skip ci]

--- js/test/fabric/event-handlers/mint.js
+++ js/test/fabric/event-handlers/mint.js
@@ -5,12 +5,8 @@ const u = require('../../../util')
 
 async function handleMint(platformState, secretStore, txnid, cm, data,
                           nextRoot) {
-  console.log('got data', data)
   const info = decodeData(data)
-  console.log('info pubkey       ', info.a_pk)
-  console.log('secretstore pubkey', secretStore.getPublicKey())
   if (info.a_pk.equals(secretStore.getPublicKey())) {
-    console.log('adding to secretstore')
     secretStore.addNoteInfo(
       cm,
       info.a_pk,
