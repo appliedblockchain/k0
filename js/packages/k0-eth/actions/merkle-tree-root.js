235dc9ee73bf7ee37dd6ce67eaf92f7cbe121254
Mario Gemoll <mario@appliedblockchain.com>
2019-06-18 11:22:37 +0100

    tiny change to trigger republishing

--- js/packages/k0-eth/actions/merkle-tree-root.js
+++ js/packages/k0-eth/actions/merkle-tree-root.js
@@ -6,6 +6,7 @@ async function merkleTreeRoot(web3, mvppt, server) {
     mvppt.methods.root(0).call(),
     mvppt.methods.root(1).call()
   ])
+
   return server.unpack256Bits(
     new BN((root[0] || 0).toString()),
     new BN((root[1] || 0).toString())
