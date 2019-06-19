9b1c3f35487fe75516b649c16a298a4cf74baa54
Mario Gemoll <mario@appliedblockchain.com>
2019-06-19 20:33:49 +0100

    removed remaining (Fabric) demo code

--- js/demo/init-event-handlers.js
+++
@@ -1,9 +0,0 @@
-const handleDeposit = require('./event-handlers/deposit')
-const handleTransfer = require('./event-handlers/transfer')
-
-function initEventHandlers(platformState, secretStore, platform) {
-  platform.on('deposit', handleDeposit.bind(null, platformState))
-  platform.on('transfer', handleTransfer.bind(null, platformState, secretStore))
-}
-
-module.exports = initEventHandlers
